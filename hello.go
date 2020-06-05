ade2391b65d6dfc0cd7c571afa303f2b17d2e9c0
Frank Hofmann <frank.hofmann@efho.de>
2020-06-05 16:37:18 +0200

    hello world added

---
+++ hello.go
@@ -0,0 +1,17 @@
+# ----------------------------------------------------
+# (C) 2020 Frank Hofmann <frank.hofmann@efho.de>
+#
+# hello-spack. A Hello, world! in Go in order 
+# to test the SPACK package manager (http://spack.io/)
+#
+# Published unter BSD License.
+# ----------------------------------------------------
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, world!")
+}
+
